9bbbb1487a6cc82c457d1f18f33d1c46ba654ace
Yazine MRAIZIG <ymraizig@a205-01.iut.univ-paris8.fr>
2019-04-19 10:56:11 +0200

    projet Gestionproj

---
+++ src/Entreprise.java
@@ -0,0 +1,12 @@
+
+public class Entreprise {
+	private String nom;
+	private String adresse;
+	private String siege;
+	
+	public Entreprise(String n,String adr,String s) {
+		nom=n;
+		adresse=adr;
+		siege=s;
+	}
+}
---
+++ src/OffreStage.java
@@ -0,0 +1,8 @@
+
+public class OffreStage {
+	private int id;
+	private String descriptif;
+	private String titre;
+	private String dateDeb;
+	private String duree;
+}
